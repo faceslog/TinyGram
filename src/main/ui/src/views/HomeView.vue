5fc43c71efcf7dc38ba9163d4debfbfa74527215
faceslog <66552663+faceslog@users.noreply.github.com>
2022-11-01 07:04:18 -0700

    Front-End Like button

--- src/main/ui/src/views/HomeView.vue
+++ src/main/ui/src/views/HomeView.vue
@@ -10,7 +10,8 @@
         <div v-for="post in posts" :key="post" class="mx-auto w-4/5 mb-4">
           
           <div class="feed-item border border-gray-400 rounded bg-white">
-            <div class="border-b p-2 flex justify-between items-center">
+            <!-- Post Header -->
+            <div class="border-b p-2 flex justify-between items-center cursor-pointer">
               
               <div class="left flex flex-row items-center">
                 <div class="user-img h-10 w-10 border rounded-full overflow-hidden mr-4">
@@ -43,9 +44,12 @@
 
                   <div class="left flex flex-row">
                     <!-- like btn -->
-                    <button class="like mr-4">
-                      <svg aria-label="Like" fill="#262626" height="24" viewBox="0 0 48 48" width="24">
+                    <button class="like mr-4" @click="likePost(post)">
+                      <svg v-if="!post.hasLiked" aria-label="Like" fill="#262626" viewBox="0 0 48 48" height="26" width="26">
                         <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
+                      </svg>
+                      <svg v-else araria-label="Like" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" width="30" height="30">
+                        <path fill="#f44336" d="M9.727273 2.090909c-1.1454548 0-2.1545457.572727-2.727273 1.472727-.5727273-.9-1.5818182-1.472727-2.7272727-1.472727C2.4727273 2.090909 1 3.563636 1 5.363636c0 3.245455 6 6.545455 6 6.545455s6-3.272727 6-6.545455c0-1.8-1.472727-3.272727-3.272727-3.272727z"/>
                       </svg>
                     </button>
 
@@ -122,6 +126,7 @@ export default {
           hasLiked: false,
           timeStamp: Date.now(),
           imgUrl: "https://images.unsplash.com/photo-1667144842815-1c740881a905?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+          postUrl: "",
           lastComment: {
             username: "Gabriel",
             content: "Salut c'est un test"
@@ -132,9 +137,10 @@ export default {
           userpic: "https://i.pinimg.com/originals/c2/4a/af/c24aaf49f7dc286dd0f7020a5bb820ac.png",
           location: "Université de Nantes",
           likesCount: 156,
-          hasLiked: false,
+          hasLiked: true,
           timeStamp: Date.now(),
           imgUrl: "https://images.unsplash.com/photo-1667206742636-2f7ae349a3f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+          postUrl: "",
           lastComment: {
             username: "Toto",
             content: "Salut c'est un test 2"
@@ -145,6 +151,22 @@ export default {
   },
   components: {
     Header
+  },
+  methods: {
+    likePost: async function(post) 
+    {
+      // Si par défaut le post n'est pas like on incrémente
+      if(!post.hasLiked)
+        post.likesCount++;
+      else
+        post.likesCount--;
+
+      // On actualise le status de hasLiked.
+      post.hasLiked = !post.hasLiked;
+      
+      // Augmenter ou décrementer le compteur dans le backend ...
+    }
+    
   }
 }
 </script>
