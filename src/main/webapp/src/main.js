928df43bcb8e70f23c700c34efd3cefdda93c521
faceslog <66552663+faceslog@users.noreply.github.com>
2022-11-04 06:40:56 -0700

    Add axios to call backend api

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 import './assets/index.css'
 
 const app = createApp(App)
 
 app.use(router)
+
+// Setup Axios
+const instance = axios.create({
+    baseURL: 'https://tinyinsta-366314.nw.r.appspot.com/_ah/api/instapi/v1',
+});
+
+app.config.globalProperties.axios = instance;
+// ---------------------------
 
 app.mount('#app')
