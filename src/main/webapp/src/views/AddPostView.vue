71a8b51754450aa5b36acab144546154dbca5004
faceslog <66552663+faceslog@users.noreply.github.com>
2022-11-06 11:07:10 -0800

    Add post request to upload post

--- src/main/webapp/src/views/AddPostView.vue
+++ src/main/webapp/src/views/AddPostView.vue
@@ -2,30 +2,30 @@
   
   <Header></Header>
     
-  <div class="w-full overflow-scroll h-screen bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 p-4 flex items-center justify-center" >
+  <div class="w-full h-screen bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 p-4 flex items-center justify-center" >
     <div class="bg-white py-6 px-10 sm:max-w-md w-full ">
       <div class="sm:text-3xl text-2xl font-semibold text-center text-sky-600  mb-12">Post your Picture (Dev)</div>
 
       <div class="">
         <div>
-          <input type="text" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500"  placeholder="Url"/>
+          <input v-model="url" type="text" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500"  placeholder="Url"/>
         </div>
 
         <div>
-          <input type="text" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8"  placeholder="Location (Optional)" maxlength="80"/>
+          <input v-model="location" type="text" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 my-8"  placeholder="Location (Optional)" maxlength="80"/>
         </div>
 
         <div>
-          <input type="text" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 mb-8"  placeholder="Description (Optional)" maxlength="80"/>
+          <input v-model="description" type="text" class="focus:outline-none border-b w-full pb-2 border-sky-400 placeholder-gray-500 mb-8"  placeholder="Description (Optional)" maxlength="80"/>
         </div>
 
         <div class="flex">
-          <input type="checkbox" class="border-sky-400" value=""/>
+          <input type="checkbox" class="border-sky-400" v-model="hasAgreed"/>
           <div class="px-3 text-gray-500">I accept terms & conditions</div>
         </div>
 
         <div class="flex justify-center my-6">
-          <button class="rounded-full p-3 w-full sm:w-56   bg-gradient-to-r from-sky-600  to-teal-300 text-white text-lg font-semibold">Publish</button>
+          <button @click="post" class="rounded-full p-3 w-full sm:w-56 bg-gradient-to-r from-sky-600  to-teal-300 text-white text-lg font-semibold">Publish</button>
         </div>
       </div>
     </div>
@@ -36,10 +36,61 @@
 <script>
 import Header from '@/components/Header.vue'
 
-
 export default {
   components: {
     Header
+  },
+  data()  {
+    return {
+      url: '',
+      location: '',
+      description: '',
+      hasAgreed: false,
+      isLoading: false
+    }
+  },
+  methods: {
+    verifyUrl: function() {
+      
+      const regExp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?\.(png|jpg|jpeg)/;
+
+      return regExp.test(this.url);
+    },
+    post: async function() {
+
+      if(this.isLoading) return;
+
+      if(!this.verifyUrl()) {
+        this.$swal('Invalid Image', 'Must point to a .png .jpg .jpeg', 'error');
+        return;
+      }
+      
+      if(!this.hasAgreed) {
+        this.$swal('Oops...', 'You must agree to the terms', 'error');
+        return;
+      }
+
+      // Toggle loading to true to prevent spamming the post button
+      this.isLoading = true;
+
+      let token = this.$store.getters.getToken;
+      this.url = encodeURI(this.url);
+
+      this.$axios.post(`/add?access_token=${token}&image=${this.url}`).then(res => {
+
+        console.log(res);
+        let userId = res.data.owner.key;
+        let postId = res.data.key;
+
+        this.$router.push(`/community/${userId}/${postId}`);    
+
+      }).catch(err => {
+        this.isLoading = false;
+        console.log(err);
+        this.$swal('Failed to upload', 'Oops something went wrong', 'error');
+      });
+
+    },
   }
 }
 </script>
